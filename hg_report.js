84b1e911ab004b2a086babc31e99c12a763d8596
Chris Kim <ckim990@west-mec.org>
2019-03-04 15:22:15 -0700

    Changed names of files

---
+++ hg_report.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: 
+   Date:   
+   
+   Filename: hg_report.js
+	
+*/
+
+var gameReport = "<h1>" + itemTitle + "</h1> <h2> By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "'id='gameImg'/> <table> <tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr> </table>" + itemSummary;
+
+
